734dfb433c6c44413d6e346be9f03d3647b515bc
Håkon Hjelde Wold <hakon@wtw.no>
2017-07-14 11:59:49 +0200

    Optimized tails in LinearSeqOptimized Fixes scala/bug/#10304

--- test/junit/scala/collection/LinearSeqOptimizedTest.scala
+++ test/junit/scala/collection/LinearSeqOptimizedTest.scala
@@ -16,4 +16,20 @@ class LinearSeqOptimizedTest {
     assertEquals(2, "abcde".toList.indexWhere(_ == 'c', -1))
     assertEquals(2, "abcde".toList.indexWhere(_ == 'c', -2))
   }
+
+  @Test def test_efficientTails_list_SI9892: Unit = {
+    val tails = List(1,2,3,4).tails.toList
+
+    assertEquals(tails(0).tail, tails(1))
+    assertEquals(tails(0).tail.tail, tails(2))
+    assertEquals(tails(1).tail, tails(2))
+    assertEquals(tails(3).tail, tails(4))
+    assertEquals(tails(4), List())
+  }
+
+  @Test def test_efficientTails_stream_SI9892: Unit = {
+    val stream = Stream.from(1)
+    val tails = stream.tails.toStream
+    assertTrue(tails.head eq stream)
+  }
 }
